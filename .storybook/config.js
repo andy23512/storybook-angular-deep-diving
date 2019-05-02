9f2475e16a0ca0372f77b54e80d86360c2d0e5cf
Tangent Chang <andy23512@gmail.com>
2019-05-02 11:17:24 +0800

    Set dark theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,22 @@
-import { configure } from '@storybook/angular';
+import {
+  configure,
+  addParameters
+} from '@storybook/angular';
+import {
+  themes
+} from '@storybook/theming';
 
 // automatically import all files ending in *.stories.ts
 const req = require.context('../src/stories', true, /\.stories\.ts$/);
+
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+  options: {
+    theme: themes.dark,
+  },
+});
 
 configure(loadStories, module);
