efd59211aa337580a0b022bb54f79585c5c8fa75
Tangent Chang <andy23512@gmail.com>
2019-05-03 12:53:47 +0800

    Move task-list component to containers folder

--- taskbox/src/app/tasks/task.module.ts
+++ taskbox/src/app/tasks/task.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgxsModule } from '@ngxs/store';
 
 import { TaskComponent } from './components/task.component';
-import { TaskListComponent } from './components/task-list.component';
+import { TaskListComponent } from './containers/task-list.component';
 import { PureTaskListComponent } from './components/pure-task-list.component';
 import { TasksState } from './state/task.state';
 
